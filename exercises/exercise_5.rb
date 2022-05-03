0d9a669f7279127012efdf119ccc2c9de77f568c
Nicole MacLean <nicole.mac0404@gmail.com>
2022-05-02 20:01:06 -0700

    Add revenue cala, ex_5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(&:annual_revenue)
+total_stores = Store.count
+avg_revenue = total_revenue / total_stores
+puts "Total revenue: #{total_revenue}"
+puts "Average revenue: #{avg_revenue}"
+
+milli_stores = Store.where("annual_revenue > 1000000").count
+puts "This many millis #{milli_stores}"
